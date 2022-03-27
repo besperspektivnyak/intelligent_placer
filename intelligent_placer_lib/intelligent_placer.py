680ba991412b6d4d285ff30b8551a660a3b48618
besperspektivnyak <elizaveta-gr@mail.ru>
2022-03-27 17:52:19 +0300

    first version

---
+++ intelligent_placer_lib/intelligent_placer.py
@@ -0,0 +1,14 @@
+from intelligent_placer_lib.objects_detection import read_image, preprocess_image, find_contours, find_min_rect, find_approx_polygon
+from intelligent_placer_lib.objects_arrangemant import is_objects_placed
+
+
+def check_image(path: str) -> bool:
+    image = read_image(path)
+    edge = preprocess_image(image)
+    polygon, objects = find_contours(edge)
+    if polygon is None or objects == []:
+        print("Can't find polygon or objects")
+        return False
+    rectangles = find_min_rect(objects)
+    polygon_angles = find_approx_polygon(polygon)
+    return is_objects_placed(polygon_angles, rectangles)
